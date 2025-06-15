3009ea4a29b82c06171fe49424f8938e31fb2761
Sydroid01 <aronsydc@gmail.com>
2025-06-15 17:12:37 +0800

    Delete BibleTrackerApp-Maven-Patch directory

--- BibleTrackerApp-Maven-Patch/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.openjfx.BibleReadingTrackerFX {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires java.xml;
-	requires javafx.graphics;
-
-    opens org.openjfx.BibleReadingTrackerFX to javafx.fxml;
-    exports org.openjfx.BibleReadingTrackerFX;
-}
